9372b2eb726cce11929e4622ae440405be724287
Ivana Vukmirov <ivanav32001@gmail.com>
2023-11-02 22:43:10 +0100

    Prva inicijalizacija

---
+++ model/test.php
@@ -0,0 +1,9 @@
+<?php
+    $ime="Ivana";
+    $odgovor="Logovani korisnik je $ime";
+    $odgovor2='Logovani korisnik je $ime';
+    $odgovor3='Logovani korisnik je '. $ime;
+
+    echo $odgovor;
+
+?>
